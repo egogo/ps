7365b970d11b3e1426da15bbade05f4125c4a88d
Anton Sokol <ego@Zawerty.local>
2009-10-23 19:11:20 +0300

    jpg + 90

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,8 +1,8 @@
 require 'mime/types'
 class Photo < ActiveRecord::Base
   belongs_to  :gallery
-  has_attached_file :image, :styles => { :gal => ["700x466>", :png], :thumb => ["48x48#", :png], :admin_thumb => ["70x47>", :png], :admin_prev => ["140x94>", :png] },
-                    :convert_options => { :all=> "-quality 80" },
+  has_attached_file :image, :styles => { :gal => ["700x466>", :jpg], :thumb => ["48x48#", :jpg], :admin_thumb => ["70x47>", :jpg], :admin_prev => ["140x94>", :jpg] },
+                    :convert_options => { :all=> "-quality 90" },
                     :url  => "/images/gallery/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/images/gallery/:id/:style/:basename.:extension"
 
