59dfadf4919e1ba4ac67576fdaf847d3538dbf8a
Anton Sokol <ego@Zawerty.local>
2009-10-23 19:02:11 +0300

    compression options

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,7 @@ require 'mime/types'
 class Photo < ActiveRecord::Base
   belongs_to  :gallery
   has_attached_file :image, :styles => { :gal => ["700x466>", :jpg], :thumb => ["48x48#", :jpg], :admin_thumb => ["70x47>", :jpg], :admin_prev => ["140x94>", :jpg] },
+                    :convert_options => { :all=> "-quality 80" },
                     :url  => "/images/gallery/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/images/gallery/:id/:style/:basename.:extension"
 
