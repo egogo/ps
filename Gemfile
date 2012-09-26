0bcd6d1d51f9460f2f9777f8fdcd9609371094d1
Anton Sokol <anton.sokol@gmail.com>
2012-09-26 17:01:21 +0300

    Gitfile

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source :rubygems
+gem 'rails', '=2.3.11'
+gem 'sqlite3'
+gem 'mime-types'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,66 +1,67 @@
+require 'cuoco/capistrano'
 
-  set :application, "photoshaft"
-  set :domain, "ec2-50-16-102-158.compute-1.amazonaws.com"
-  role :app, domain
-  role :web, domain
-  role :db, domain, :primary => true
-  set :deploy_to, "/var/www/apps/#{application}"
-  set :user, "root"
-  # set :password, ""
-  set :group, "root"
- 
-  set :scm, "git"
-  set :repository, "git://github.com/egogo/ps.git"
-  set :branch, "master"  
-  set :deploy_via, :checkout
-  set :git_shallow_clone, 1
-  default_run_options[:pty] = true
-  # ssh_options[:keys] = %w(/root/.ssh/id_dsa)
-  
-  namespace :deploy do
-    desc "Restarting mod_rails with restart.txt"
-    task :restart, :roles => :app, :except => { :no_release => true } do
-      run "touch #{current_path}/tmp/restart.txt"
-    end
+set :application, "photoshaft"
+set :domain, "69.55.49.169"
+role :app, domain
+role :web, domain
+role :db, domain, :primary => true
+set :deploy_to, "/var/www/apps/#{application}"
+set :user, "root"
+# set :password, ""
+set :group, "root"
 
-    desc "Stop task is a deploy.web.disable with mod_rails"
-    task :stop, :roles => :app do
-      deploy.web.disable
-    end
+set :scm, "git"
+set :repository, "git://github.com/egogo/ps.git"
+set :branch, "master"  
+set :deploy_via, :checkout
+set :git_shallow_clone, 1
+default_run_options[:pty] = true
+# ssh_options[:keys] = %w(/root/.ssh/id_dsa)
 
-    desc "Start task is a deploy.web.enable with mod_rails"
-    task :start, :roles => :app do
-      deploy.web.enable
-    end
-  end  
-  
-  after "deploy:update_code",
-                              :symlink_config_files,
-                              :link_database,
-                              :fix_public_dir_permission,
-                              :fix_tmp_dir_permission,
-                              "deploy:migrate"
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
 
-  task :symlink_config_files do
-    run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml"
-    run "rm -rf #{release_path}/public/images/gallery"
-    run "ln -s #{deploy_to}/#{shared_dir}/assets/gallery #{release_path}/public/images"
-    sudo "chmod -R a+rw #{release_path}/public/images/gallery"
+  desc "Stop task is a deploy.web.disable with mod_rails"
+  task :stop, :roles => :app do
+    deploy.web.disable
   end
- 
-  task :link_database do
-    run "ln -nfs #{deploy_to}/#{shared_dir}/db/production.sqlite3 #{release_path}/db/production.sqlite3"
-    run "chmod g+w #{release_path}/db/production.sqlite3"
+
+  desc "Start task is a deploy.web.enable with mod_rails"
+  task :start, :roles => :app do
+    deploy.web.enable
   end
- 
-  task :fix_public_dir_permission do
-    run "chgrp -R root #{release_path}/public"
-    run "chmod -R g+w #{release_path}/public"
-  end
- 
-  task :fix_tmp_dir_permission do
-    run "chgrp -R root #{release_path}/tmp"
-    sudo "chown -R root #{release_path}/tmp"
-    sudo "chmod -R a+rw #{release_path}/tmp"
-  end
-  
+end  
+
+after "deploy:update_code",
+                            :symlink_config_files,
+                            :link_database,
+                            :fix_public_dir_permission,
+                            :fix_tmp_dir_permission,
+                            "deploy:migrate"
+
+task :symlink_config_files do
+  run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml"
+  run "rm -rf #{release_path}/public/images/gallery"
+  run "ln -s #{deploy_to}/#{shared_dir}/assets/gallery #{release_path}/public/images"
+  sudo "chmod -R a+rw #{release_path}/public/images/gallery"
+end
+
+task :link_database do
+  run "ln -nfs #{deploy_to}/#{shared_dir}/db/production.sqlite3 #{release_path}/db/production.sqlite3"
+  run "chmod -R 0777 #{release_path}/db"
+  # run "chmod g+w #{release_path}/db/production.sqlite3"
+end
+
+task :fix_public_dir_permission do
+  run "chgrp -R root #{release_path}/public"
+  run "chmod -R g+w #{release_path}/public"
+end
+
+task :fix_tmp_dir_permission do
+  run "chgrp -R root #{release_path}/tmp"
+  sudo "chown -R root #{release_path}/tmp"
+  sudo "chmod -R a+rw #{release_path}/tmp"
+end
